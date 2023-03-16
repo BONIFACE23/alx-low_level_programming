845ce91e3da9d1029b85e38df1dec56b76dfe79d
BONIFACE23 <irunguboniface413@gmail.com>
2023-03-16 11:31:53 -0700

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main-main function
+ * Return:always 0
+ **/
+int main(void)
+{
+        printf("Size of a char:%lu byte(s)",size of(char));
+        printf("Size of an int:%lu byte(s)",size of(int));
+        printf("size of a long long int:%lu byte(s)",size of(long long int));
+        printf("size of a float:%lu byte(s)",size of (float));
+        return (0);
+}
