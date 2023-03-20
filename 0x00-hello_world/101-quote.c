820fa17e78ed5c30a44a4a81d1e72e8658b69581
BONIFACE23 <irunguboniface413@gmail.com>
2023-03-20 10:10:48 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,11 @@
 #include<unistd.h>
 /**
  * main-main function
- * Function:always 0
+ *Return:succession (1)
  **/
 int main(void)
 {
-write(2, "and that piece of art is useful\"  - Dora Korpar, 2015 - 10 - 19\n", 90);
+	fputs("and that piece of art is useful"
+			"\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
