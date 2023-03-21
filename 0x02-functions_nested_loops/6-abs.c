58d05881811d6c5a5d2ca3b4ca0b3e58bf6b1ed9
BONIFACE23 <irunguboniface413@gmail.com>
2023-03-21 10:43:42 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include"main.h"
 /**
  *  _abs -  computes the absolute value of an integer
- *  @int: The character
+ *  @r: The character
  *  Return: the absolute value of an integer
  **/
 int _abs(int r)
