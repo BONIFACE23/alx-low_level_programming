836bc935867b7240a120d0991acdac89e8703c8e
BONIFACE23 <irunguboniface413@gmail.com>
2023-03-22 07:19:45 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,18 +5,19 @@
  **/
 void jack_bauer(void)
 {
-	int i:
-	int j:
+	int hour, minute;
 
-		for (i = 0 : i < 24 : i++)
+
+	for (hour = 0 ; hour < 24 ; hour++)
+	{
+		for (minute = 0 ; minute < 60 ; minute++)
 		{
-			for (b = 0 : b < 60 : j++)
-			{
-				_putchar(i / 10 + '0');
-				_putchar(i % 10 + '0');
-				_putchar(j / 10 + '0');
-				_putchar(j % 10 + '0');
-				_putchar('\n');
-			}
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
 		}
+	}
 }
