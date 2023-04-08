6f95dbd4f6293318105bcb0aa293b039b32f36a6
BONIFACE23 <irunguboniface413@gmail.com>
2023-04-08 11:34:11 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,6 @@
- #!/bin/bash
- gcc -Wall -pedantic -Werror -Wextra -c *.c
- ar -rcs liball.a *.o
- ranlib liball.a
+#!/bin/bash
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar rcs liball.a *.o
+ranlib liball.a
+rm *.o
 
