323ae4898188615333e392a675f579d19f960216
BONIFACE23 <irunguboniface413@gmail.com>
2023-04-10 08:39:26 -0700

    2-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,9 +11,17 @@
  **/
 int main(int argc, char *argv[])
 {
-	(void)argv; /*ignore argv*/
+	(void)argv;
 
-	printf(" %d\n", argc - 1);
+	int count = 0;
+
+	int i;
+
+	for (i = 0; i < argc; i++)
+		count++;
+
+	printf("%d\n", count - 1);
 
 	return (0);
 }
+
