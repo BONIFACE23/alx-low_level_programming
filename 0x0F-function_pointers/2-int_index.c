08a70f9b7fd4818e0f604aec643ba7ba3e15d48f
BONIFACE23 <irunguboniface413@gmail.com>
2023-04-19 02:47:32 -0700

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include<stdio.h>
+/**
+ *int_index - function that searches for integer
+ *@array:array
+ *@size:size of an array
+ *@cmp:poiner to function of one 3 main
+ *Return: 0
+ **/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
+}
+
