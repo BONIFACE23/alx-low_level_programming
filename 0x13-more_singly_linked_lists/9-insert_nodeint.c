00b6cd307c8c7a400f15d6aa60998c3ad2bd1a52
BONIFACE23 <irunguboniface413@gmail.com>
2023-05-03 14:34:38 -0700

    9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,50 +1,51 @@
-#include <stdio.h>
+#include "lists.h"
 #include <stdlib.h>
 
-typedef struct listint_s 
-{
-	int n;
-	struct listint_s *next;
-} listint_t;
-
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position
- * @head: Pointer to the head of the linked list
- * @idx: Index of the list where the new node should be added
- * @n: Value to be assigned to the new node
+ * insert_nodeint_at_index - inserts a new node at a given position
+ * @head: pointer to a pointer to the first node of the list
+ * @idx: index of the position where the new node
+ * @n: integer value of the new node
  *
- * Return: Address of the new node, or NULL if it failed
+ * Return: pointer to the new node, or NULL if it failed
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current_node;
+	listint_t *new_node, *prev_node;
 	unsigned int i;
 
-	if (head == NULL)
+	if (!head)
 		return (NULL);
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
+	/* create new node */
+	new_node = malloc(sizeof(*new_node));
+	if (!new_node)
 		return (NULL);
-
 	new_node->n = n;
-	if (idx == 0) {
+
+	/* special case for idx = 0 (insert at beginning) */
+	if (idx == 0)
+	{
 		new_node->next = *head;
 		*head = new_node;
 		return (new_node);
 	}
 
-	current_node = *head;
-	for (i = 0; i < idx - 1 && current_node != NULL; i++)
-		current_node = current_node->next;
+	/* traverse the list until the node before idx */
+	prev_node = *head;
+	for (i = 0; i < idx - 1 && prev_node; i++)
+		prev_node = prev_node->next;
 
-	if (current_node == NULL) {
+	/* check if the traversal was successful */
+	if (!prev_node)
+	{
 		free(new_node);
 		return (NULL);
 	}
 
-	new_node->next = current_node->next;
-	current_node->next = new_node;
+	/* insert the new node after prev_node */
+	new_node->next = prev_node->next;
+	prev_node->next = new_node;
 
 	return (new_node);
 }
