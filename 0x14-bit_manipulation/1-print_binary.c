ae66519b11b70877aa7012ccd65d349a081f3401
BONIFACE23 <irunguboniface413@gmail.com>
2023-05-04 18:33:40 -0700

    1-print_binary.c

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_binary - prints the binary representation of a number
+ *
+ * @n: unsigned long int to be printed in binary
+ *
+ * Return: void
+ */
+void print_binary(unsigned long  int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	putchar((n & 1) ? '1' : '0');
+}
+
